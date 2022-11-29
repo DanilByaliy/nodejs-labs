f5d9c9603bf73a1550af7b8c507aa24fe76f45b9
DanilByaliy <byaliy03@gmail.com>
2022-11-29 23:58:10 +0200

    Add routes to handle methods

--- src/api.js
+++ src/api.js
@@ -4,5 +4,9 @@ import controller from './controllers/controller.js'
 const router = new Router()
 
 router.get('/api/get', controller.get)
+router.post('/api', controller.post)
+router.patch('/api', controller.patch)
+router.delete('/api', controller.deleteHandler)
+router.options('/api', controller.options)
 
 export default router
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,8 +1,58 @@
+const contentType = 'application/json'
+
 function get(req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.json('success')
 }
 
+function post(req, res, url, payload, rawRequest) {
+  res.writeHead(200, { 'Content-Type': contentType })
+  res.json({
+    pathname: url.pathname,
+    method: req.method,
+    message: 'POST method',
+    payload,
+    rawRequest,
+  })
+}
+
+function patch(req, res, url, payload, rawRequest) {
+  res.writeHead(200, { 'Content-Type': contentType })
+  res.json({
+    pathname: url.pathname,
+    method: req.method,
+    message: 'PATCH method',
+    payload,
+    rawRequest,
+  })
+}
+
+function deleteHandler(req, res, url, payload, rawRequest) {
+  res.writeHead(200, { 'Content-Type': contentType })
+  res.json({
+    pathname: url.pathname,
+    method: req.method,
+    message: 'DELETE method',
+    payload,
+    rawRequest,
+  })
+}
+
+function options(req, res, url, payload, rawRequest) {
+  res.writeHead(200, { 'Content-Type': contentType })
+  res.json({
+    pathname: url.pathname,
+    method: req.method,
+    message: 'OPTIONS method',
+    payload,
+    rawRequest,
+  })
+}
+
 export default {
   get,
+  post,
+  patch,
+  deleteHandler,
+  options,
 }
